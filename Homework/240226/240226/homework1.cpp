b1174d2f3634a92f5f41365b08cfdbd45527bbaf
pchanbro <142496245+pchanbro@users.noreply.github.com>
2024-02-28 15:51:49 +0900

    240226 숙제

--- Homework/240226/240226/homework1.cpp
+++ Homework/240226/240226/homework1.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-
 // 로또생성기
-// 1. 1 ~ 45까지 적힌 볼을 만든다.
-// 2. array를 섞는다
-// 3. 그 중 7개를 뽑는다.
 
+// 1. 게임시작 알림판
 void PrintStartLotto()
 {
 	printf("┌───────────────────────────────────┐\n");
@@ -16,41 +13,51 @@ void PrintStartLotto()
 	cout << endl;
 }
 
-void GenerateNumber(int *arr1)
+// 2. 1 ~ 45까지 적힌 볼을 만든다.
+void GenerateNumber(int *arr)
 {
 	for (int i = 0; i < 45; i++)
 	{
-
+		arr[i] = i + 1;
 	}
 }
 
-void main()
+// 3. array를 섞는다
+void SuffleArr(int* arr)
 {
-	PrintStartLotto();
-
-	int arr1[45] = {};
-
-	GenerateNumber(arr1);
-
-	
-
-	for (int i = 0; i < 45; i++)
-	{
-		arr1[i] = i + 1;
-	}
-
 	for (int i = 0; i < 100000; i++)
 	{
 		int firstIndex = rand() % 45;
 		int secondIndex = rand() % 45;
 
-		int temp = arr1[firstIndex];
-		arr1[firstIndex] = arr1[secondIndex];
-		arr1[secondIndex] = temp;
+		int temp = arr[firstIndex];
+		arr[firstIndex] = arr[secondIndex];
+		arr[secondIndex] = temp;
 	}
+}
 
+// 4. 그 중 7개를 뽑는다.
+void Choose7Number(int* arr)
+{
 	for (int i = 0; i < 7; i++)
 	{
-		printf("%d번째 숫자 : %d\n", i + 1, arr1[i]);
+		printf("%d번째 숫자 : %d\n", i + 1, arr[i]);
 	}
+}
+
+void main()
+{
+	srand(time(NULL));
+	// 1. 게임시작 알림판
+	PrintStartLotto();
+
+	// 2. 1 ~ 45까지 적힌 볼을 만든다.
+	int arr1[45] = {};
+	GenerateNumber(arr1);
+
+	// 3. array를 섞는다
+	SuffleArr(arr1);
+
+	// 4. 그 중 7개를 뽑는다.
+	Choose7Number(arr1);
 }
