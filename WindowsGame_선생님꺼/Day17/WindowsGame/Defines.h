ee9a9a04c20ab0c2ed6e5cd6db8771a89645ae70
pchanbro <142496245+pchanbro@users.noreply.github.com>
2024-04-03 09:04:07 +0900

    240329 복습

---
+++ WindowsGame_선생님꺼/Day17/WindowsGame/Defines.h
@@ -0,0 +1,16 @@
+#pragma once
+//매크로 추가해줄거에요.
+
+#define DECLARE_SINGLE(ClassName)			\
+private:									\
+	ClassName() {}							\
+	~ClassName() {}							\
+public:										\
+	static ClassName* GetInstance()			\
+	{										\
+		static ClassName instance;			\
+		return &instance;					\
+	}
+
+
+#define GET_SINGLE(ClassName)	ClassName::GetInstance()
